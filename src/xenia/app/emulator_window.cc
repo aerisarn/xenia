077ba537ea17b8ccf233ada44027e585a3b36c74
Edoardo Canepa <aerisarn@gmail.com>
2024-01-03 00:58:22 +0100

    bump 1.1.5

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -2426,11 +2426,12 @@ void EmulatorWindow::WinRTFrontendDialog::OnDraw(ImGuiIO& io) {
 
       if (ImGui::BeginTabItem("About", nullptr)) {
         ImGui::TextWrapped(
-            "Xenia UWP 1.1.4\nA fork of Xenia introducing Xbox support and a big "
+            "Xenia UWP 1.1.5\nA fork of Xenia introducing Xbox support and a big "
             "picture frontend.\n\n"
             "Xenia's Website: https://xenia.jp/\n"
             "Xenia's Patreon: https://www.patreon.com/xenia_project\n\n"
             "This Xbox fork was developed by SirMangler.\n"
+            "updated by Aerisarn.\n"
             "Support me on Ko-Fi: https://ko-fi.com/sirmangler\n"
             "Source Code: https://github.com/SirMangler/xenia/\n\n"
             "Big thanks to TXF for creating the background, as well as to "
